cb7373b8f0d60c03d49ee11f415d09d26196285a
teclong-mac.mini <fires.wang@qq.com>
2020-09-25 10:17:54 +0800

    定时任务机制框架搭建完成

--- app/cmd/GupiaoCmd.class.php
+++ app/cmd/GupiaoCmd.class.php
@@ -5,6 +5,7 @@ use \Json;
 use \Err;
 use \Fun;
 use \TB;
+use \cmd\service\GupiaoService;
 
 class GupiaoCmd
 {
@@ -12,5 +13,8 @@ class GupiaoCmd
 
         $v1 = Fun::logic__src('www.aa.com', 'AA.BB.CC');
         var_dump($v1);
+
+        $o1 = new GupiaoService;
+        $o1->f1();
     }
 }
---
+++ app/cmd/service/GupiaoService.class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace cmd\service;
+use \Json;
+use \Err;
+use \Fun;
+use \TB;
+
+class GupiaoService
+{
+    public function f1(){
+
+        var_dump('service');
+    }
+}
